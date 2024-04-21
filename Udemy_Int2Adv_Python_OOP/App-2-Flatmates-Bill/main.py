cd13bf6fc8973eb43a64fb499032f52a7da77c1e
Sarah M. Eggleston <sarahmeggleston@gmail.com>
2024-04-20 22:13:30 -0400

    populating class methods

--- Udemy_Int2Adv_Python_OOP/App-2-Flatmates-Bill/main.py
+++ Udemy_Int2Adv_Python_OOP/App-2-Flatmates-Bill/main.py
@@ -1,3 +1,6 @@
+from fpdf import FPDF
+import webbrowser
+
 class Bill:
     """
     Object that contains data about a bill, such as
@@ -18,8 +21,9 @@ class Flatmate:
         self.name = name
         self.days_in_house = days_in_house
 
-    def pays(self, bill):
-        pass
+    def pays(self, bill, flatmate2):
+        weight = self.days_in_house /(self.days_in_house + flatmate2.days_in_house)
+        return round(bill.amount * weight, 2)
 
 
 class PdfReport:
@@ -32,4 +36,40 @@ class PdfReport:
         self.filename = filename
 
     def generate(self, flatmate1, flatmate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        # add icon
+        pdf.image(name='files/house.png', w=30, h=30)
+
+        # insert title
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Flatmates Bill', border=0, ln=1, align='C')
+
+        # insert Period label and value
+        pdf.set_font(family='Times', size=14, style='B')
+        pdf.cell(w=100, h=40, txt='Period:', border=0)
+        pdf.cell(w=150, h=40, txt=bill.period, border=0, ln=1)
+
+        # insert name and amount due for flatmate1
+        pdf.set_font(family='Times', size=12)
+        pdf.cell(w=100, h=25, txt=flatmate1.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(flatmate1.pays(bill, flatmate2)), border=0, ln=1)
+
+        # insert name and amount due for flatmate2
+        pdf.cell(w=100, h=25, txt=flatmate2.name, border=0)
+        pdf.cell(w=150, h=25, txt=str(flatmate2.pays(bill, flatmate1)), border=0)
+
+        # create pdf
+        pdf.output(self.filename)
+        webbrowser.open(self.filename)
+
+
+the_bill = Bill(120, "March 2021")
+john = Flatmate("John", 20)
+mary = Flatmate("Mary", 25)
+
+print("John pays: {}".format(john.pays(the_bill, mary)))
+print("Mary pays: {}".format(mary.pays(the_bill, john)))
+pdf = PdfReport(filename='Bill.pdf')
+pdf.generate(flatmate1=john, flatmate2=mary, bill=the_bill)
